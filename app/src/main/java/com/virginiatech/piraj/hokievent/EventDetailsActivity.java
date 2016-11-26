f1968fc1b0e300a626ca12c17e6ff21388dbca00
Rikkokiri <pilvi.rajala@gmail.com>
2016-11-25 19:41:18 -0500

    Working on EventDetails skeleton

--- app/src/main/java/com/virginiatech/piraj/hokievent/EventDetailsActivity.java
+++ app/src/main/java/com/virginiatech/piraj/hokievent/EventDetailsActivity.java
@@ -2,12 +2,32 @@ package com.virginiatech.piraj.hokievent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EventDetailsActivity extends AppCompatActivity {
+
+    //TODO eventID ?
+
+    private TextView eventName;
+    private TextView eventDate;
+    private TextView eventTime;
+    private TextView eventAddress;
+    private TextView eventDescription;
+
+    //TODO More elements...
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        eventName = (TextView) findViewById(R.id.eventName);
+        eventDate = (TextView) findViewById(R.id.eventDate);
+        eventTime = (TextView) findViewById(R.id.eventTime);
+        eventAddress = (TextView) findViewById(R.id.eventAddress);
+        eventDescription = (TextView) findViewById(R.id.eventDescription);
+
+        //TODO Pull data from the "server" and write it into TextViews
+
     }
 }
