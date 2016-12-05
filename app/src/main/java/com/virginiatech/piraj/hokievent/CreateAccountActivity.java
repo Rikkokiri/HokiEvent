ec73bb2eddb6b57209532190273013e0f4adee88
Rikkokiri <pilvi.rajala@gmail.com>
2016-12-05 00:51:55 -0500

    Bare bones of navigation bar added

--- app/src/main/java/com/virginiatech/piraj/hokievent/CreateAccountActivity.java
+++ app/src/main/java/com/virginiatech/piraj/hokievent/CreateAccountActivity.java
@@ -70,7 +70,8 @@ public class CreateAccountActivity extends AppCompatActivity {
             // Open SelectInterestsActivity to allow user to select their interests
             //Intent selectInterests = new Intent(view.getContext(), InterestsActivity.class);
             //Intent selectInterests = new Intent(view.getContext(), EventDetailsActivity.class);
-            Intent selectInterests = new Intent(view.getContext(), CreateEventActivity.class);
+            //Intent selectInterests = new Intent(view.getContext(), CreateEventActivity.class);
+            Intent selectInterests = new Intent(view.getContext(), EventDetailsActivity.class);
             startActivity(selectInterests);
         }
     };
--- app/src/main/java/com/virginiatech/piraj/hokievent/EventDetailsActivity.java
+++ app/src/main/java/com/virginiatech/piraj/hokievent/EventDetailsActivity.java
@@ -1,12 +1,20 @@
 package com.virginiatech.piraj.hokievent;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.IdRes;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 public class EventDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -24,10 +32,15 @@ public class EventDetailsActivity extends AppCompatActivity implements OnMapRead
     private MapFragment mapFragment;
     private GoogleMap map;
 
+    // --- Bottom bar ---
+    BottomBar bottomBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        buildBottomBar(this, savedInstanceState);
 
         eventName = (TextView) findViewById(R.id.eventName);
         eventDate = (TextView) findViewById(R.id.eventDate);
@@ -66,4 +79,72 @@ public class EventDetailsActivity extends AppCompatActivity implements OnMapRead
 
         }
     }
+
+    /**
+     * Build navigation bar located on the bottom of the screen.
+     *
+     * @param activity
+     * @param savedInstanceState
+     */
+    private void buildBottomBar(Activity activity, Bundle savedInstanceState){
+
+        bottomBar = BottomBar.attach(activity, savedInstanceState,
+                ContextCompat.getColor(this, R.color.colorPrimary), // Background Color
+                ContextCompat.getColor(this, R.color.colorAccent), // Tab Item Color
+                0.25f); // Tab Item Alpha
+        bottomBar.setItems(R.menu.navigation_bar_items);
+
+
+        bottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                navigate(menuItemId);
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                Log.i("NavBar","Menu item selected");
+            }
+        });
+
+    }
+
+    /**
+     *
+     * @param menuID
+     */
+    private void navigate(int menuID){
+
+        switch (menuID){
+            case R.id.action_home:
+                Intent goHomeIntent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(goHomeIntent);
+                break;
+
+            case R.id.action_create_event:
+                Intent createEventIntent = new Intent(getApplicationContext(), CreateEventActivity.class);
+                startActivity(createEventIntent);
+                break;
+
+            case R.id.action_search:
+                Intent searchIntent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(searchIntent);
+                break;
+
+            case R.id.action_profile:
+                Intent profileIntent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(profileIntent);
+                break;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Necessary to restore the BottomBar's state, otherwise we would
+        // lose the current tab on orientation change.
+        bottomBar.onSaveInstanceState(outState);
+    }
+
 }
