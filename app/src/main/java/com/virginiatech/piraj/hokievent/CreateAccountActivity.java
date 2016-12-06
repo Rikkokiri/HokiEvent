320758b30a35074999018c600d5a3fcab0a775d2
Rikkokiri <pilvi.rajala@gmail.com>
2016-12-06 04:54:21 -0500

    Send new user to the server

--- app/src/main/java/com/virginiatech/piraj/hokievent/CreateAccountActivity.java
+++ app/src/main/java/com/virginiatech/piraj/hokievent/CreateAccountActivity.java
@@ -20,6 +20,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
@@ -58,6 +59,15 @@ public class CreateAccountActivity extends AppCompatActivity {
     static final String PASS = "pass";
     static final String CONFIRM_PASS = "confirm pass";
 
+    //JSON keys
+    private static final String FIRTS_NAME_JSON = "firstName";
+    private static final String MIDDLE_NAME_JSON = "middleName";
+    private static final String LAST_NAME_JSON = "lastName";
+    private static final String EMAIL_JSON = "userEmail";
+    private static final String PHONE_NUMBER_JSON = "phoneNumber";
+    private static final String INTERESTS_JSON = "interests";
+    private static final String PASSWORD_JSON = "password";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,10 +103,6 @@ public class CreateAccountActivity extends AppCompatActivity {
         interests = "";
 
         interestsView.setText("Select interests");
-
-        APICaller api = new APICaller();
-        try { api.APIpostUser(new JSONObject("{\"userEmail\":\"kyz@vt.edu\",\"firstName\":\"Kevin\",\"lastName\":\"Zhang\",\"phoneNumber\":\"540-776-3851\",\"interests\":\"Food, Gaming\"}"));}
-        catch(Exception e) {System.out.println(e);}
 
         //TODO Add functionality to button listeners
 
@@ -162,8 +168,29 @@ public class CreateAccountActivity extends AppCompatActivity {
 
 
             //TODO Send server new user entry.  Retrieve new userID from server.
+            JSONObject json = new JSONObject();
 
-            user.setUserID(0 + "");
+            try {
+                json.put(FIRTS_NAME_JSON, user.getFirstName());
+                json.put(MIDDLE_NAME_JSON, user.getMiddleName());
+                json.put(LAST_NAME_JSON, user.getMiddleName());
+                json.put(EMAIL_JSON, user.getUserEmail());
+                json.put(PHONE_NUMBER_JSON, user.getPhoneNumber());
+                json.put(INTERESTS_JSON, user.getInterests());
+                json.put(PASSWORD_JSON, user.getPassword());
+
+            } catch (JSONException exception){
+                //TODO Handle exception
+            }
+
+            APICaller api = new APICaller();
+            try {
+                api.APIpostUser(json);
+            } catch(Exception e) {
+                System.out.println(e);
+            }
+
+            user.setUserID(0 + ""); //TODO ?
 
             try {
 
